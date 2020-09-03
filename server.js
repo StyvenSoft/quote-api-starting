3272404c9c4c131f9561f5340805aa7fa85c3f42
Steveen <steveen.echeverri@gmail.com>
2020-09-03 12:51:33 -0500

    Get a random quote from the imported

--- server.js
+++ server.js
@@ -11,3 +11,10 @@ app.use(express.static('public'));
 app.listen(PORT, () => {
     console.log(`Server is listening on http://localhost:${PORT}`);
 });
+
+app.get('/api/quotes/random', (req, res, next) => {
+    console.log('GET Request Received');
+    res.status(200).send({
+        quote: getRandomElement(quotes)
+    });
+});
